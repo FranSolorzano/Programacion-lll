8e9cda1c20aeb48a025732fbd35d341c89f71778
English <FranSolorzano>
2023-02-08 15:47:07 -0600

    Uploads

---
+++ fran/inmuebles/inmuebles/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inmuebles
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona persona = new Persona();
+            Console.WriteLine("Por favor ingrese su nombre");
+            persona.nombre = Console.ReadLine();
+            Console.WriteLine("Por favor ingrese su apellido");
+            persona.apellido = Console.ReadLine();
+            Console.Clear();
+
+            Console.WriteLine("\nBIENVENIDO SEA A NUESTRA VENTA DE INMUEBLES " + " " + persona.nombre + " "+persona.apellido);
+            int opcion;
+
+            Console.WriteLine("********************************MENU DE INMUEBLES***********************************");
+            Console.WriteLine("¿Que tipo de inmueble deseas adquirir?" + persona);
+            Console.WriteLine("1-- CASA DE PLAYA");
+            Console.WriteLine("2-- DEPARTAMENTO");
+            Console.WriteLine("3-- PARECELAS");
+            Console.WriteLine("4-- EDIFICIO");
+            opcion = int.Parse(Console.ReadLine());
+
+            switch (opcion)
+            {
+                case 1:
+                    Console.Clear();
+                    
+                    Console.WriteLine("Casa de playa");
+                    break;
+            }
+            
+        }
+        
+            
+    }
+
+     class Persona
+    {
+        public string id { get; set; }
+        public string dui { get; set; }
+        public string nombre { get; set; }
+        public string apellido { get; set; }
+        public string edad { get; set; }
+        public string direccion { get; set; }
+
+
+    }
+
+    class Cliente : Persona 
+    {
+        public int NumTelefonico;
+        
+    }   
+
+    class Empleados :Persona
+    {
+
+    }
+
+    class Inmuebles
+    {
+
+    }
+
+    class Contrato
+    {
+
+    }
+}
---
+++ fran/practica 2/practica 2/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practica_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //distanciamos las clases creando variables para poder mandarla a llamar
+
+            Persona persona1 = new Persona();
+
+            //aca le aplicamos valor a nuestras variables y las imprimimos
+
+            persona1.Nombre = "Franklin";
+            Console.WriteLine(persona1.Nombre);
+
+            Estudiante estudiante= new Estudiante();
+            estudiante.Nombre = persona1.Nombre + " NOÉ";
+            Console.WriteLine(estudiante.Nombre);
+            Console.ReadLine();
+        }
+    }
+
+    class Persona
+    {
+        //declaramos variables si las hacemos privadas no las podemos distanciar fuera del metodo
+
+        private string nombre;
+        public string Nombre
+        {
+            get{ return nombre;}
+            set { nombre= value;}
+        }
+        public int edad;
+        public string peso;
+        public string altura;
+
+        //declaramos metodos
+
+        public void Comer()
+        {
+            Console.WriteLine("Soy capaz de comer todo tipo de comida");
+        }
+
+        public void Caminar()
+        {
+            Console.WriteLine("Soy capaz de moverme");
+        }
+    }
+
+    class Estudiante : Persona 
+    {
+       public void Aprender()
+        {
+            Console.WriteLine("Aprendo programacion");
+        }
+    }
+
+}
